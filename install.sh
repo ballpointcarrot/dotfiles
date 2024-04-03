1c2922fee24b0f44213280416ccd781e13eaba15
Christopher Kruse <ckruse@ballpointcarrot.net>
2024-04-02 22:35:02 -0700

    fix: attempt to preload linuxbrew if present
    We use a standard linuxbrew location for my containers, so we want to
    explicitly check if that file is present, and eval/load the brew env
    as part of the load for the container.

--- install.sh
+++ install.sh
@@ -5,15 +5,25 @@ set -ex
 # Ensure XDG config directory is present
 mkdir -p ~/.config
 
-# install starship for that statusline bling
-if [ -n "$(which starship)" ]; then
-  # assume using Bash in a docker context
-  echo 'eval "$(starship init bash)"' > ~/.bashrc
+# Check for a brew install, and do the 
+# base brew package set
+container_brew_path="/home/linuxbrew/.linuxbrew/bin/brew"
+if [ -z "$(which brew)"]; then
+  if [ -f "$container_brew_path" ]; then
+    eval "$($container_brew_path shellenv)"
+  fi
 fi
 
 if [ -n "$(which brew)" ]; then
   cp Brewfile ~/.Brewfile
   brew bundle --global
+fi
+
+# install starship for that statusline bling
+# execute after brew, as it's probably installing starship :)
+if [ -n "$(which starship)" ]; then
+  # assume using Bash in a docker context
+  echo 'eval "$(starship init bash)"' > ~/.bashrc
 fi
 
 # Copy editor configurations
