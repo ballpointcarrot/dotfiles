20e1f163823a5c8a18255636598c9515d0fad194
Christopher Kruse <ckruse@ballpointcarrot.net>
2024-10-03 22:25:17 -0700

    fix: replace Astro with LazyVim
    Found LazyVim to be a generally better experience for prepackaged VIM
    setups.

--- install.sh
+++ install.sh
@@ -5,37 +5,36 @@ set -ex
 # Ensure XDG config directory is present
 mkdir -p ~/.config
 
-# Check for a brew install, and do the 
+# Check for a brew install, and do the
 # base brew package set
 container_brew_path="/home/linuxbrew/.linuxbrew/bin/brew"
 if [ -z "$(which brew)"]; then
-  if [ -f "$container_brew_path" ]; then
-    eval "$($container_brew_path shellenv)"
-  fi
+	if [ -f "$container_brew_path" ]; then
+		eval "$($container_brew_path shellenv)"
+	fi
 fi
 
 if [ -n "$(which brew)" ]; then
-  cp Brewfile ~/.Brewfile
-  brew bundle --global
+	cp Brewfile ~/.Brewfile
+	brew bundle --global
 fi
 
 # install starship for that statusline bling
 # execute after brew, as it's probably installing starship :)
 if [ -n "$(which starship)" ]; then
-  echo 'eval "$(starship init bash)"' >> ~/.profile
+	echo 'eval "$(starship init bash)"' >>~/.profile
 fi
 
 # install zoxide shell actions
 if [ -n "$(which zoxide)" ]; then
-  echo 'eval "$(zoxide init bash)"' >> ~/.profile
+	echo 'eval "$(zoxide init bash)"' >>~/.profile
 fi
 # Copy editor configurations
 cp -R .config/helix ~/.config/
 
-# Install Astro NVIM
-git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
+# Install LazyVim:
+git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
 # Copy $HOMEDIR-level files necessary for runtime
 cp gitconfig ~/.gitconfig
-
